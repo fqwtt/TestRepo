0528d7cea72d98748045c534920e3aaca874e0b3
Your Name <you@example.com>
2023-04-17 14:18:21 +0800

    cuda

--- CudaTest/test.cu
+++ CudaTest/test.cu
@@ -1,58 +1,65 @@
-
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
 #include <iostream>
+#include <string>
+#define THREAD_PER_BLOCK 256
 
 using namespace std;
-// Kernel definition
+
 class Person {
 public:
+	Person() = default;
+
+	Person(int age, double height) {
+		this->age = age;
+		this->height = height;
+	}
 	int age = 10;
+	double height = 9.9;
 };
-__global__ void VecAdd(int* A, int* B, int* C)
+// Kernel definition
+__global__ void PersonAddKernel(Person* A, Person* B, Person* C)
 {
 	int i = threadIdx.x;
-	C[i] = A[i] + B[i];
-	printf("%d,%d\n", threadIdx.y, threadIdx.x);
+	int j = blockIdx.x;
+	int id = j * THREAD_PER_BLOCK + i;
+	C[id].age = A[id].age + B[id].age;
+	C[id].height = A[id].height + B[id].height;
 }
-__global__ void PersonAdd(Person* A, Person* B, Person* C)
-{
-	int i = threadIdx.x;
-	C[i].age = A[i].age + B[i].age;
-	printf("%d,%d\n", threadIdx.y, threadIdx.x);
-}
+
 int main()
 {
-	int* dev_a = 0;
-	int* dev_b = 0;
-	int* dev_c = 0;
-	int size = 3;
+	Person* dev_a = NULL;
+	Person* dev_b = NULL;
+	Person* dev_c = NULL;
+
+	int block_size = 2;
+	int thread_size = THREAD_PER_BLOCK;
+	int size = block_size * thread_size;
+	cudaSetDevice(0);
+	unique_ptr<Person[]> A = make_unique<Person[]>(size);
+	unique_ptr<Person[]> B = make_unique<Person[]>(size);
+	unique_ptr<Person[]> C = make_unique<Person[]>(size);
 
 
-	cudaError_t cudaStatus = cudaSetDevice(0);
-	if (cudaStatus != cudaSuccess) {
-		cout << "cudaSetDevice failed!  Do you have a CUDA-capable GPU installed?" << endl;
-		//exit(0);
-	}
-	Person A[3];
-	Person B[3];
-	Person C[3];
-	cudaMalloc((void**)&dev_c, size * sizeof(int));
-	cudaMalloc((void**)&dev_a, size * sizeof(int));
-	cudaMalloc((void**)&dev_b, size * sizeof(int));
-	cudaMemcpy(dev_a, A, size * sizeof(int), cudaMemcpyHostToDevice);
-	cudaMemcpy(dev_b, B, size * sizeof(int), cudaMemcpyHostToDevice);
-	dim3 threadsPerBlock(3, 2);
+	cudaMalloc((void**)&dev_c, size * sizeof(Person));
+	cudaMalloc((void**)&dev_a, size * sizeof(Person));
+	cudaMalloc((void**)&dev_b, size * sizeof(Person));
+	cudaMemcpy(dev_a, A.get(), size * sizeof(Person), cudaMemcpyHostToDevice);
+	cudaMemcpy(dev_b, B.get(), size * sizeof(Person), cudaMemcpyHostToDevice);
 
+	//system("pause");
 	// N 个线程的内核调用
-	VecAdd << <1, threadsPerBlock >> > (dev_a, dev_b, dev_c);
+	PersonAddKernel << <block_size, thread_size >> > (dev_a, dev_b, dev_c);
 	cudaDeviceSynchronize();
-	cudaMemcpy(C, dev_c, size * sizeof(int), cudaMemcpyDeviceToHost);
+	cudaMemcpy(C.get(), dev_c, size * sizeof(Person), cudaMemcpyDeviceToHost);
 	for (int i = 0; i < size; i++) {
-		cout << C[i].age << endl;
+		cout << C[i].age << " " << C[i].height << endl;
 	}
-	cudaFree(dev_c);
+	
 	cudaFree(dev_a);
 	cudaFree(dev_b);
+	cudaFree(dev_c);
+	cudaDeviceReset();
 	return 0;
 }
--- Learn/main.cpp
+++ Learn/main.cpp
@@ -23,52 +23,14 @@ using namespace std;
 
 class Person {
 public:
-	Person() = default;
-	Person(string name, int age) {
-		this->name = name;
-		this->age = age;
-	}
-	~Person() {
-		cout << "xigou" << endl;
-	}
-	string name;
-	int age;
-	bool operator==(const Person& obj) {
-		if (this->name == obj.name && this->age == obj.age) {
-			return true;
-		}
-		else {
-			return false;
-		}
-	}
+	Person(const std::string& name, int age) : name_(name), age_(age) {}
+	std::string name_;
+	int age_;
 };
 
-template<typename T>
-class smart_pointer {
-private:
-	T* m_RawPointer;
-public:
-	smart_pointer(T* p) :m_RawPointer(p) {};
-	~smart_pointer() { delete m_RawPointer; };
-	T operator=(const T* p) {
-		this->m_RawPointer = p;
-	}
-	T& operator*() {
-		return *m_RawPointer;
-	}
-	T* operator->() {
-		return m_RawPointer;
-	}
-};
+
 int main() {
 
-
-	unique_ptr<Person[]> p(new Person[2]);
-	////Person* p1 = new Person[2133300];
-
-	//system("pause");
-	Person* p1 = p.get();
-	system("pause");
 	return 0;
 }
 
